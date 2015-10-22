9fae4a3ec240c6bd78fe35457654d3ed8dd1bcfa
David Sleeckx <david@2sky.be>
2015-10-22 15:57:07 +0200

    Prepare for Vidyano.Web2 dist project

--- gruntfile.js
+++ gruntfile.js
@@ -28,16 +28,47 @@ module.exports = function(grunt) {
                     fast: 'never'
                 }
             }
+        },
+        clean: ["dist/Vidyano.Web2/src/"],
+        copy: {
+            dist: {
+                cwd: "src",
+                src: "**",
+                dest: "dist/Vidyano.Web2/src/",
+                expand: true,
+                filter: function (src) {
+                    if (src.indexOf(".less") > 0 || src.indexOf(".js") > 0 || src.indexOf(".html") > 0)
+                        return true;
+
+                    return false;
+                }
+            }
+        },
+        dtsGenerator: {
+            options: {
+                name: 'Vidyano',
+                baseDir: 'src',
+                out: 'dist/Vidyano.Web2/vidyano.d.ts'
+            },
+            default: {
+                src: ['src/**/*.ts']
+            }
         }
     });
 
     grunt.registerTask("default", [
         "bower:install",
         "less",
-        "ts"
+        "ts",
+        "clean",
+        "copy:dist",
+        "dtsGenerator"
     ]);
 
     grunt.loadNpmTasks("grunt-bower-task");
     grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-ts");
+    grunt.loadNpmTasks('dts-generator');
 };
