5b7fc9e1b8331c99351bfdfe21300b60eca94c89
David Sleeckx <david@2sky.be>
2015-10-30 11:50:16 +0100

    Prepare for 0.7.4-alpha

--- dist/Vidyano.Web2/Properties/AssemblyInfo.cs
+++ dist/Vidyano.Web2/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.3.4")]
-[assembly: AssemblyFileVersion("0.7.3.4")]
-[assembly: AssemblyInformationalVersion("0.7.3.4-alpha")]
+[assembly: AssemblyVersion("0.7.4.0")]
+[assembly: AssemblyFileVersion("0.7.4.0")]
+[assembly: AssemblyInformationalVersion("0.7.4.0-alpha")]
