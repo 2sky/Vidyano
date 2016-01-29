59f85968115e84226abcb76c67f602ff93264eee
David Sleeckx <david@2sky.be>
2016-01-29 14:56:35 +0100

    Trigger refresh from password attributes on blur

--- src/WebComponents/Attributes/PersistentObjectAttributePassword/persistent-object-attribute-password.ts
+++ src/WebComponents/Attributes/PersistentObjectAttributePassword/persistent-object-attribute-password.ts
@@ -1,5 +1,9 @@
 module Vidyano.WebComponents.Attributes {
     @PersistentObjectAttribute.register
     export class PersistentObjectAttributePassword extends WebComponents.Attributes.PersistentObjectAttribute {
+        private _editInputBlur() {
+            if (this.attribute && this.attribute.isValueChanged && this.attribute.triggersRefresh)
+                this.attribute.setValue(this.value = this.attribute.value, true);
+        }
     }
 }
