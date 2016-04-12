32ea0275e21256fcf36dd9e4c99d5bd763f98ad0
David Sleeckx <david@2sky.be>
2016-04-12 16:30:10 +0200

    Allow Dropdown, Enum and KeyValueList as radio buttons

--- src/WebComponents/Attributes/PersistentObjectAttributeDropDown/persistent-object-attribute-drop-down.ts
+++ src/WebComponents/Attributes/PersistentObjectAttributeDropDown/persistent-object-attribute-drop-down.ts
@@ -1,11 +1,36 @@
 namespace Vidyano.WebComponents.Attributes {
     "use strict";
 
-    @PersistentObjectAttribute.register
+    @PersistentObjectAttribute.register({
+        properties: {
+            radio: {
+                type: Boolean,
+                computed: "_computeRadio(attribute)"
+            }
+        }
+    })
     export class PersistentObjectAttributeDropDown extends WebComponents.Attributes.PersistentObjectAttribute {
         protected _valueChanged(newValue: any) {
             if (this.attribute && newValue !== this.attribute.value)
                 this.attribute.setValue(newValue, true);
+        }
+
+        private _computeRadio(attribute: Vidyano.PersistentObjectAttribute): boolean {
+            return attribute && attribute.getTypeHint("inputtype", undefined, undefined, true) === "radio";
+        }
+
+        private _isRadioChecked(option: string, value: string): boolean {
+            return option === value || (!option && !value);
+        }
+
+        private _radioLabel(option: string): string {
+            return option != null ? option : "—";
+        }
+
+        private _radioChanged(e: CustomEvent) {
+            e.stopPropagation();
+
+            this.attribute.setValue((<any>e).model.option, true);
         }
     }
 }
--- src/WebComponents/Attributes/PersistentObjectAttributeKeyValueList/persistent-object-attribute-key-value-list.ts
+++ src/WebComponents/Attributes/PersistentObjectAttributeKeyValueList/persistent-object-attribute-key-value-list.ts
@@ -1,11 +1,36 @@
 namespace Vidyano.WebComponents.Attributes {
     "use strict";
 
-    @PersistentObjectAttribute.register
+    @PersistentObjectAttribute.register({
+        properties: {
+            radio: {
+                type: Boolean,
+                computed: "_computeRadio(attribute)"
+            }
+        }
+    })
     export class PersistentObjectAttributeKeyValueList extends WebComponents.Attributes.PersistentObjectAttribute {
         protected _valueChanged(newValue: any) {
             if (this.attribute && newValue !== this.attribute.value)
                 this.attribute.setValue(newValue, true);
+        }
+
+        private _computeRadio(attribute: Vidyano.PersistentObjectAttribute): boolean {
+            return attribute && attribute.getTypeHint("inputtype", undefined, undefined, true) === "radio";
+        }
+
+        private _isRadioChecked(option: Vidyano.Common.IKeyValuePair, value: string): boolean {
+            return option == null && value == null || (option && option.key === value);
+        }
+
+        private _radioLabel(option: Vidyano.Common.IKeyValuePair): string {
+            return option.value != null ? option.value : "—";
+        }
+
+        private _radioChanged(e: CustomEvent) {
+            e.stopPropagation();
+
+            this.attribute.setValue((<any>e).model.option.key, true);
         }
     }
 }
