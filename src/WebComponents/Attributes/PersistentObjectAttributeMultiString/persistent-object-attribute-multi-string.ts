f532f12239f19ab1ac27f81eec208234f97b146b
David Sleeckx <david@sleeckx.be>
2016-11-21 15:59:39 +0100

    Fixed readonly issue on update
    Untabify file

--- src/WebComponents/Attributes/PersistentObjectAttributeMultiString/persistent-object-attribute-multi-string.ts
+++ src/WebComponents/Attributes/PersistentObjectAttributeMultiString/persistent-object-attribute-multi-string.ts
@@ -1,79 +1,75 @@
 namespace Vidyano.WebComponents.Attributes {
     "use strict";
 
-	@Sortable.register
-	export class PersistentObjectAttributeMultiStringItems extends Sortable {
-		protected _dragEnd() {
-			this.fire("reorder-strings", {}, { bubbles: true });
-		}
-	}
+    @Sortable.register
+    export class PersistentObjectAttributeMultiStringItems extends Sortable {
+        protected _dragEnd() {
+            this.fire("reorder-strings", {}, { bubbles: true });
+        }
+    }
 
-	@WebComponent.register({
+    @WebComponent.register({
         properties: {
             value: {
-				type: String,
-				observer: "_valueChanged"
-			},
-			isReadOnly: {
-				type: Boolean,
-				reflectToAttribute: true
-			},
-			isNew: {
-				type: Boolean,
-				reflectToAttribute: true
-			},
-			disabled: {
-				type: Boolean,
-				reflectToAttribute: true
-			},
+                type: String,
+                observer: "_valueChanged"
+            },
+            isReadOnly: {
+                type: Boolean,
+                reflectToAttribute: true
+            },
+            isNew: {
+                type: Boolean,
+                reflectToAttribute: true
+            },
+            disabled: {
+                type: Boolean,
+                reflectToAttribute: true
+            },
             placeholder: String
-		}
+        }
     })
     export class PersistentObjectAttributeMultiStringItem extends WebComponent {
-		private _focusQueued: boolean;
-		value: string;
-		isNew: boolean;
-		isReadOnly: boolean;
+        private _focusQueued: boolean;
+        isNew: boolean;
+        isReadOnly: boolean;
 
-		constructor(value: string, isReadOnly: boolean = false) {
-			super();
+        constructor(public value: string) {
+            super();
+        }
 
-			this.value = value;
-			this.isReadOnly = isReadOnly;
-		}
+        attached() {
+            super.attached();
 
-		attached() {
-			super.attached();
+            if (this._focusQueued) {
+                this._focusQueued = false;
+                this.$["input"].focus();
+            }
+        }
 
-			if (this._focusQueued) {
-				this._focusQueued = false;
-				this.$["input"].focus();
-			}
-		}
+        queueFocus() {
+            this._focusQueued = true;
+        }
 
-		queueFocus() {
-			this._focusQueued = true;
-		}
+        private _valueChanged(value: string) {
+            if (this.isReadOnly)
+                return;
 
-		private _valueChanged(value: string) {
-			if (this.isReadOnly)
-				return;
+            if (this.isNew) {
+                if (value) {
+                    this.fire("multi-string-item-value-new", { value: value });
+                    this.value = "";
+                }
+            }
+            else
+                this.fire("multi-string-item-value-changed", null);
+        }
 
-			if (this.isNew) {
-				if (value) {
-					this.fire("multi-string-item-value-new", { value: value });
-					this.value = "";
-				}
-			}
-			else
-				this.fire("multi-string-item-value-changed", null);
-		}
-
-		private _onInputBlur() {
-			if (!this.isReadOnly && !this.isNew)
-				this.fire("multi-string-item-value-changed", null);
-		}
-	}
+        private _onInputBlur() {
+            if (!this.isReadOnly && !this.isNew)
+                this.fire("multi-string-item-value-changed", null);
+        }
+    }
 
     @PersistentObjectAttribute.register({
         properties: {
@@ -83,63 +79,69 @@ namespace Vidyano.WebComponents.Attributes {
                 computed: "_computeStrings(value, attribute.isReadOnly)"
             }
         },
-		observers: [
-			"_render(strings, editing, isAttached)"
-		],
-		listeners: {
-			"multi-string-item-value-new": "_itemValueNew",
-			"multi-string-item-value-changed": "_itemValueChanged",
-			"reorder-strings": "_itemsOrderChanged"
-		}
+        observers: [
+            "_render(strings, editing, isAttached)"
+        ],
+        listeners: {
+            "multi-string-item-value-new": "_itemValueNew",
+            "multi-string-item-value-changed": "_itemValueChanged",
+            "reorder-strings": "_itemsOrderChanged"
+        },
+        forwardObservers: [
+            "attribute.isReadOnly"
+        ]
     })
     export class PersistentObjectAttributeMultiString extends PersistentObjectAttribute {
-		strings: PersistentObjectAttributeMultiStringItem[];
-		private _setNewString: (newString: PersistentObjectAttributeMultiStringItem) => void;
+        strings: PersistentObjectAttributeMultiStringItem[];
+        private _setNewString: (newString: PersistentObjectAttributeMultiStringItem) => void;
 
-		private _computeStrings(value: string, readOnly: boolean): PersistentObjectAttributeMultiStringItem[] {
-			return value ? value.split("\n").filter(v => !!v.length).map((v: string, n: number) => this.strings && this.strings[n] && this.strings[n].value === v ? this.strings[n] : new PersistentObjectAttributeMultiStringItem(v, readOnly)) : [];
-		}
+        private _computeStrings(value: string, readOnly: boolean): PersistentObjectAttributeMultiStringItem[] {
+            const strings = value ? value.split("\n").filter(v => !!v.length).map((v: string, n: number) => this.strings && this.strings[n] && this.strings[n].value === v ? this.strings[n] : new PersistentObjectAttributeMultiStringItem(v)) : [];
+            strings.forEach(s => s.isReadOnly = readOnly);
 
-		private _itemValueNew(e: Event, detail: { value: string }) {
-			this.value = `${this.value || ""}\n${detail.value}`;
-			this.strings[this.strings.length - 1].$["input"].focus();
+            return strings;
+        }
 
-			e.stopPropagation();
-		}
+        private _itemValueNew(e: Event, detail: { value: string }) {
+            this.value = `${this.value || ""}\n${detail.value}`;
+            this.strings[this.strings.length - 1].$["input"].focus();
 
-		private _itemsOrderChanged() {
-			const stringsContainer = <HTMLElement>Polymer.dom(this.root).querySelector("#strings");
-			this.value = Enumerable.from(stringsContainer.querySelectorAll("input")).where((i: HTMLInputElement) => !!i.value).select((i: HTMLInputElement) => i.value).toArray().join("\n");
-		}
+            e.stopPropagation();
+        }
 
-		private _itemValueChanged(e: Event) {
-			this.value = this.strings.filter(s => !!s.value).map(s => s.value).join("\n");
+        private _itemsOrderChanged() {
+            const stringsContainer = <HTMLElement>Polymer.dom(this.root).querySelector("#strings");
+            this.value = Enumerable.from(stringsContainer.querySelectorAll("input")).where((i: HTMLInputElement) => !!i.value).select((i: HTMLInputElement) => i.value).toArray().join("\n");
+        }
 
-			e.stopPropagation();
-		}
+        private _itemValueChanged(e: Event) {
+            this.value = this.strings.filter(s => !!s.value).map(s => s.value).join("\n");
 
-    	private _getValues(): string[] {
-    		return Array.prototype.map.apply(this.querySelectorAll("#inputs > vi-resource"), [resource => resource.model.value]);
-    	}
+            e.stopPropagation();
+        }
 
-		private _render(strings: PersistentObjectAttributeMultiStringItem[], editing: boolean, isAttached: boolean) {
-			if (!editing || !isAttached)
-				return;
+        private _getValues(): string[] {
+            return Array.prototype.map.apply(this.querySelectorAll("#inputs > vi-resource"), [resource => resource.model.value]);
+        }
 
-			Polymer.dom(this).flush();
+        private _render(strings: PersistentObjectAttributeMultiStringItem[], editing: boolean, isAttached: boolean) {
+            if (!editing || !isAttached)
+                return;
 
-			const stringsContainer = <HTMLElement>Polymer.dom(this.root).querySelector("#strings");
-			const diff = stringsContainer.children.length !== strings.length || strings.some((s, n) => stringsContainer.children[n] !== s);
+            Polymer.dom(this).flush();
 
-			strings.forEach((s: PersistentObjectAttributeMultiStringItem, index: number) => {
-				if (diff)
-					Polymer.dom(stringsContainer).appendChild(s);
-			});
+            const stringsContainer = <HTMLElement>Polymer.dom(this.root).querySelector("#strings");
+            const diff = stringsContainer.children.length !== strings.length || strings.some((s, n) => stringsContainer.children[n] !== s);
 
-			Enumerable.from(stringsContainer.children).toArray().forEach((c: PersistentObjectAttributeMultiStringItem) => {
-				if (strings.indexOf(c) < 0)
-					Polymer.dom(stringsContainer).removeChild(c);
-			});
-		}
+            strings.forEach((s: PersistentObjectAttributeMultiStringItem, index: number) => {
+                if (diff)
+                    Polymer.dom(stringsContainer).appendChild(s);
+            });
+
+            Enumerable.from(stringsContainer.children).toArray().forEach((c: PersistentObjectAttributeMultiStringItem) => {
+                if (strings.indexOf(c) < 0)
+                    Polymer.dom(stringsContainer).removeChild(c);
+            });
+        }
     }
 }
