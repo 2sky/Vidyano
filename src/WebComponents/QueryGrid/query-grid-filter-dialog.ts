fa8d23032663efbd2c1d311d20a7936f7ac07470
David Sleeckx <david@sleeckx.be>
2017-06-14 17:57:31 +0200

    Added custom edit query filter dialog

---
+++ src/WebComponents/QueryGrid/query-grid-filter-dialog.ts
@@ -0,0 +1,117 @@
+﻿namespace Vidyano.WebComponents {
+    "use strict";
+
+    @Dialog.register({
+        properties: {
+            persistentObject: {
+                type: Object,
+                readOnly: true
+            }
+        },
+        forwardObservers: [
+            "persistentObject.isBusy"
+        ]
+    })
+    export class QueryGridFilterDialog extends Dialog {
+        readonly persistentObject: Vidyano.PersistentObject; private _setPersistentObject: (persistentObject: Vidyano.PersistentObject) => void;
+
+        constructor(private _filters: QueryFilters, private _filter: QueryFilter) {
+            super();
+
+            this._setPersistentObject(_filter.persistentObject);
+            this.persistentObject.beginEdit();
+        }
+
+        private async _save() {
+            const isNew = this.persistentObject.isNew;
+            if (await this._filters.save(this._filter)) {
+                super.close();
+                return;
+            }
+
+            this._setPersistentObject(this._filter.persistentObject);
+        }
+
+        close(result?: any) {
+            this._filter.persistentObject.cancelEdit();
+            super.close();
+        }
+    }
+
+    @WebComponent.register({
+        properties: {
+            attribute: {
+                type: Object,
+                observer: "_attributeChanged"
+            },
+            grouping: {
+                type: String,
+                readOnly: true,
+                observer: "_groupingChanged"
+            },
+            group: {
+                type: String,
+                notify: true,
+                value: null
+            },
+            name: {
+                type: String,
+                notify: true,
+                value: null
+            }
+        },
+        observers: [
+            "_updateAttributeValue(attribute, name, group)"
+        ]
+    })
+    export class QueryGridFilterDialogName extends WebComponent {
+        readonly grouping: boolean; private _setGrouping: (grouping: boolean) => void;
+        attribute: Vidyano.PersistentObjectAttribute;
+        group: string;
+        name: string;
+
+        attached() {
+            super.attached();
+
+            this._groupingChanged(this.grouping);
+        }
+
+        private _attributeChanged(attribute: Vidyano.PersistentObjectAttribute) {
+            if (!attribute)
+                return;
+
+            const name = <string>attribute.value;
+            this._setGrouping(name ? name.contains("\n") : false);
+
+            if (!this.grouping)
+                this.name = name;
+            else {
+                const parts = name.split("\n");
+                this.group = parts[0];
+                this.name = parts[1];
+            }
+        }
+
+        private _groupingChanged(grouping: boolean, previous?: boolean) {
+            const input = <HTMLInputElement>this.$[grouping ? "groupInput" : "nameInput"];
+
+            input.selectionStart = 0;
+            if (grouping && previous !== false && input.value)
+                input.selectionEnd = input.value.length;
+            else
+                input.selectionEnd = 0;
+
+            input.focus();
+        }
+
+        private _toggleGrouping() {
+            this._setGrouping(!this.grouping);
+            if (!this.grouping)
+                this.group = "";
+        }
+
+        private _updateAttributeValue(attribute: Vidyano.PersistentObjectAttribute, name: string, group: string) {
+            attribute.setValue(name && group ? `${group}\n${name}` : name);
+        }
+    }
+}
