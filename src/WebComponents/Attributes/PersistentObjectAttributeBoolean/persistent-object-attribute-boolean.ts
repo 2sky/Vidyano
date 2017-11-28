e58a74af2c417102552daccc73c3b53b43e7979f
David Sleeckx <david@sleeckx.be>
2017-11-28 12:53:03 +0100

    Added toggle
    Use toggle as default boolean attribute component
    Added inputtype=checkbox typehints for boolean attributes

--- src/WebComponents/Attributes/PersistentObjectAttributeBoolean/persistent-object-attribute-boolean.ts
+++ src/WebComponents/Attributes/PersistentObjectAttributeBoolean/persistent-object-attribute-boolean.ts
@@ -1,15 +1,34 @@
 namespace Vidyano.WebComponents.Attributes {
     "use strict";
 
-    @PersistentObjectAttribute.register
+    @PersistentObjectAttribute.register({
+        properties: {
+            canToggle: {
+                type: Boolean,
+                computed: "_computeCanToggle(editing, readOnly)"
+            },
+            checkBox: {
+                type: Boolean,
+                computed: "_computeCheckBox(attribute)"
+            }
+        }
+    })
     export class PersistentObjectAttributeBoolean extends WebComponents.Attributes.PersistentObjectAttribute {
         protected _valueChanged(newValue: any) {
             if (this.attribute && newValue !== this.attribute.value)
                 this.attribute.setValue(newValue, true).catch(Vidyano.noop);
         }
 
+        private _computeCanToggle(editing: boolean, isReadOnly: boolean): boolean {
+            return editing && !isReadOnly;
+        }
+
         private _isDisabled(isReadOnly: boolean, isFrozen: boolean): boolean {
             return isReadOnly || isFrozen;
+        }
+
+        private _computeCheckBox(attribute: Vidyano.PersistentObjectAttribute): boolean {
+            return attribute.getTypeHint("inputtype", undefined, undefined, true) === "checkbox";
         }
     }
 
---
+++ src/WebComponents/Toggle/toggle.ts
@@ -0,0 +1,64 @@
+﻿namespace Vidyano.WebComponents {
+    "use strict";
+
+    @WebComponent.register({
+        properties: {
+            toggled: {
+                type: Boolean,
+                reflectToAttribute: true,
+                notify: true
+            },
+            label: {
+                type: String,
+                value: null
+            },
+            isNull: {
+                type: Boolean,
+                value: true,
+                computed: "_computeIsNull(toggled)"
+            },
+            disabled: {
+                type: Boolean,
+                reflectToAttribute: true
+            }
+        },
+        hostAttributes: {
+            "tabindex": "0"
+        },
+        listeners: {
+            "tap": "toggle"
+        },
+        keybindings: {
+            "space": "_keyToggle",
+            "enter": "_keyToggle"
+        }
+    })
+    export class Toggle extends WebComponents.WebComponent {
+        toggled: boolean;
+        label: string;
+        disabled: boolean;
+        radio: boolean;
+
+        toggle() {
+            if (this.disabled)
+                return;
+
+            this.toggled = !this.toggled;
+        }
+
+        private _keyToggle(e: KeyboardEvent) {
+            if (document.activeElement !== this)
+                return true;
+
+            this.toggle();
+        }
+
+        private _computeIsNull(toggled: boolean): boolean {
+            return toggled !== false && toggled !== true;
+        }
+
+        private _isEmpty(label: string): boolean {
+            return !label;
+        }
+    }
+}
