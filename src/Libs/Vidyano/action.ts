720fd2610b648d298c68007a87c606145b6c275b
David Sleeckx <david@sleeckx.be>
2018-10-17 09:17:41 +0200

    Moved execute handler logic to execute function

--- src/Libs/Vidyano/action.ts
+++ src/Libs/Vidyano/action.ts
@@ -167,7 +167,31 @@ namespace Vidyano {
                 return null;
 
             try {
-                return await this._onExecute(options);
+                let workHandlerResolve: (po: Vidyano.PersistentObject) => void;
+                let workHandlerReject: (reason?: any) => void;
+
+                const workHandler = new Promise<Vidyano.PersistentObject>((resolve, reject) => {
+                    workHandlerResolve = resolve;
+                    workHandlerReject = reject;
+                });
+
+                if (this._executeHandlers && this._executeHandlers.length > 0) {
+                    for (let i = 0; i < this._executeHandlers.length; i++) {
+                        if (this._executeHandlers[i](this, workHandler, options) === false) {
+                            workHandlerResolve(null);
+                            return workHandler;
+                        }
+                    }
+                }
+
+                try {
+                    workHandlerResolve(await this._onExecute(options));
+                }
+                catch (e) {
+                    workHandlerReject(e);
+                }
+
+                return workHandler;
             }
             catch (e) {
                 if (options.throwExceptions)
@@ -182,102 +206,76 @@ namespace Vidyano {
             if (this.definition.confirmation && (!noConfirmation) && !await this.service.hooks.onActionConfirmation(this, menuOption))
                 return null;
 
-            let workHandlerResolve: (po: Vidyano.PersistentObject) => void;
-            let workHandlerReject: (reason?: any) => void;
+            return this.owner.queueWork(async () => {
+                parameters = this._getParameters(parameters, menuOption);
 
-            const workHandler = new Promise<Vidyano.PersistentObject>((resolve, reject) => {
-                workHandlerResolve = resolve;
-                workHandlerReject = reject;
-            });
+                if (selectedItems == null && this.query) {
+                    if (this.query.selectAll.allSelected) {
+                        if (!this.query.selectAll.inverse)
+                            selectedItems = [];
+                        else
+                            selectedItems = this.query.items.filter(i => !i.isSelected);
+                    }
+                    else
+                        selectedItems = this.query.selectedItems;
 
-            if (this._executeHandlers && this._executeHandlers.length > 0) {
-                for (let i = 0; i < this._executeHandlers.length; i++) {
-                    if (this._executeHandlers[i](this, workHandler, options) === false) {
-                        workHandlerResolve(null);
-                        return workHandler;
+                    selectedItems = selectedItems.filter(i => !i.ignoreSelect);
+                }
+
+                let po = await this.service.executeAction(this._targetType + "." + this.definition.name, this.parent, this.query, selectedItems, parameters);
+                if (po) {
+                    if (po.fullTypeName === "Vidyano.Notification") {
+                        if (po.objectId != null && JSON.parse(po.objectId).dialog) {
+                            this._setNotification();
+                            this.service.hooks.onMessageDialog(NotificationType[po.notificationType], po.notification, false, this.service.hooks.service.getTranslatedMessage("OK"));
+                        }
+                        else {
+                            if (this.query && this.definition.refreshQueryOnCompleted)
+                            /* tslint:disable:no-var-keyword */ var notificationPO = po; /* tslint:enable:no-var-keyword */
+                            else
+                                this._setNotification(po.notification, po.notificationType, po.notificationDuration);
+                        }
+
+                        po = null;
+                    } else if (po.fullTypeName === "Vidyano.RegisteredStream") {
+                        this.service._getStream(po);
+                    } else if (po.fullTypeName === "Vidyano.AddReference") {
+                        const query = po.queries[0];
+                        query.parent = this.parent;
+
+                        const selectedItems = await this.service.hooks.onSelectReference(query);
+                        if (selectedItems && selectedItems.length > 0) {
+                            try {
+                                await this.service.executeAction("Query.AddReference", this.parent, query, selectedItems, { AddAction: this.name }, true);
+                            }
+                            catch (e) {
+                                this._setNotification(e);
+                            }
+
+                            if (this.query)
+                                this.query.search();
+                        }
+                    } else if (this.parent != null && (po.fullTypeName === this.parent.fullTypeName || po.isNew === this.parent.isNew) && po.id === this.parent.id && po.objectId === this.parent.objectId) {
+                        this.parent.refreshFromResult(po);
+                    } else {
+                        po.ownerQuery = this.query;
+                        po.ownerPersistentObject = this.parent;
+
+                        if (!skipOpen)
+                            this.service.hooks.onOpen(po, false, true);
                     }
                 }
-            }
 
-            try {
-                const result = await this.owner.queueWork(async () => {
-                    parameters = this._getParameters(parameters, menuOption);
+                if (this.query != null && this.definition.refreshQueryOnCompleted) {
+                    // NOTE: Don't wait for search to complete
+                    this.query.search({ keepSelection: this.definition.keepSelectionOnRefresh }).then(() => {
+                        if (notificationPO && !this.query.notification)
+                            this._setNotification(notificationPO.notification, notificationPO.notificationType, notificationPO.notificationDuration);
+                    });
+                }
 
-                    if (selectedItems == null && this.query) {
-                        if (this.query.selectAll.allSelected) {
-                            if (!this.query.selectAll.inverse)
-                                selectedItems = [];
-                            else
-                                selectedItems = this.query.items.filter(i => !i.isSelected);
-                        }
-                        else
-                            selectedItems = this.query.selectedItems;
-
-                        selectedItems = selectedItems.filter(i => !i.ignoreSelect);
-                    }
-
-                    let po = await this.service.executeAction(this._targetType + "." + this.definition.name, this.parent, this.query, selectedItems, parameters);
-                    if (po) {
-                        if (po.fullTypeName === "Vidyano.Notification") {
-                            if (po.objectId != null && JSON.parse(po.objectId).dialog) {
-                                this._setNotification();
-                                this.service.hooks.onMessageDialog(NotificationType[po.notificationType], po.notification, false, this.service.hooks.service.getTranslatedMessage("OK"));
-                            }
-                            else {
-                                if (this.query && this.definition.refreshQueryOnCompleted)
-                            /* tslint:disable:no-var-keyword */ var notificationPO = po; /* tslint:enable:no-var-keyword */
-                                else
-                                    this._setNotification(po.notification, po.notificationType, po.notificationDuration);
-                            }
-
-                            po = null;
-                        } else if (po.fullTypeName === "Vidyano.RegisteredStream") {
-                            this.service._getStream(po);
-                        } else if (po.fullTypeName === "Vidyano.AddReference") {
-                            const query = po.queries[0];
-                            query.parent = this.parent;
-
-                            const selectedItems = await this.service.hooks.onSelectReference(query);
-                            if (selectedItems && selectedItems.length > 0) {
-                                try {
-                                    await this.service.executeAction("Query.AddReference", this.parent, query, selectedItems, { AddAction: this.name }, true);
-                                }
-                                catch (e) {
-                                    this._setNotification(e);
-                                }
-
-                                if (this.query)
-                                    this.query.search();
-                            }
-                        } else if (this.parent != null && (po.fullTypeName === this.parent.fullTypeName || po.isNew === this.parent.isNew) && po.id === this.parent.id && po.objectId === this.parent.objectId) {
-                            this.parent.refreshFromResult(po);
-                        } else {
-                            po.ownerQuery = this.query;
-                            po.ownerPersistentObject = this.parent;
-
-                            if (!skipOpen)
-                                this.service.hooks.onOpen(po, false, true);
-                        }
-                    }
-
-                    if (this.query != null && this.definition.refreshQueryOnCompleted) {
-                        // NOTE: Don't wait for search to complete
-                        this.query.search({ keepSelection: this.definition.keepSelectionOnRefresh }).then(() => {
-                            if (notificationPO && !this.query.notification)
-                                this._setNotification(notificationPO.notification, notificationPO.notificationType, notificationPO.notificationDuration);
-                        });
-                    }
-
-                    return po;
-                });
-
-                workHandlerResolve(result);
-            }
-            catch (e) {
-                workHandlerReject(e);
-            }
-
-            return workHandler;
+                return po;
+            });
         }
 
         _getParameters(parameters, option) {
